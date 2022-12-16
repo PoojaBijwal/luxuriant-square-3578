b342a6843f151e6e25c42615ce8e5ef7a320d1bc
BhavikDholu <bhavikdholu1998@gmail.com>
2022-12-16 23:26:09 +0530

    fw20_0582_day-4

--- reliance_digital_clone/src/Pages/AddProduct.jsx
+++ reliance_digital_clone/src/Pages/AddProduct.jsx
@@ -1,11 +1,91 @@
 //Bhavik Dholu fw20_0582
+import React from "react";
+import { Box, Heading, Divider, FormControl, Select, FormLabel, Input, Button, } from "@chakra-ui/react";
+import {useToast}  from '@chakra-ui/react';
 
-import React from 'react'
 
-function AddProduct() {
-  return (
-    <div>AddProduct</div>
-  )
+const init = {
+  title : "",
+  stock : 0,
+  price : 0,
+  category : "",
+  image : "",
+  description : ""
 }
 
-export default AddProduct
+function AddProduct() {
+  const [detail,setDetail] = React.useState(init);
+  const toast = useToast()
+
+  const handleChange = (e)=>{
+     const {name,value} = e.target;
+     setDetail({...detail,[name]:value});
+  }
+  const handleAdd = (e)=>{
+    e.preventDefault();
+    console.log(detail);
+    setDetail(init);
+    toast({
+      description: "Product Added Successfully",
+      status: 'success',
+      duration: 2000,
+      position: 'top-right',
+      isClosable: true,
+    })
+  }
+
+const {title,stock,price,image,category,description} = detail;
+
+  return (
+    <Box mt={5}>
+      <Heading>Add Product</Heading>
+      <Divider />
+      <Box w='80%' margin='auto' border='1px solid gray' borderRadius={10} p={5} mt={5}>
+          <FormControl>
+            <FormLabel>Product Name</FormLabel>
+            <Input placeholder="Product name" name="title" value={title} onChange={handleChange}/>
+            <FormLabel>Stock</FormLabel>
+            <Input placeholder="Number of Product" name="stock" value={stock} onChange={handleChange}/>
+            <FormLabel>Price</FormLabel>
+            <Input placeholder="Product Price" name="price" value={price} onChange={handleChange}/>
+            <FormLabel>Category</FormLabel>
+            <Select placeholder="Select country" name="category" value={category} onChange={handleChange}>
+              <option value={'mobile'}>Mobile</option>
+              <option value={'mens Clothes'}>Mens Clothes</option>
+              <option value={'womens Clothes'}>Womens Clothes</option>
+              <option value={'shoes'}>Shoes</option>
+            </Select>
+            <FormLabel>Image</FormLabel>
+            <Input placeholder="Product Image url" name="image" value={image} onChange={handleChange}/>
+            <FormLabel>Description</FormLabel>
+            <Input placeholder="Product Description" name="description" value={description} onChange={handleChange}/>
+          </FormControl>
+          <Button
+            mt={4}
+            colorScheme='teal'
+            type='submit'
+            onClick={handleAdd}
+          >
+            Add Product
+          </Button>
+      </Box>
+    </Box>
+  );
+}
+
+export default AddProduct;
+
+/*
+{
+    "id": 1,
+    "title": "Qubo Baby Cam Smart Camera, Black (Penguin Themed)",
+    "price": 4990,
+    "description": "If you are photography enthusiast then Featuring an 18 MP (APS-C) CMOS sensor and the DIGIC 4+ imaging processor, you can capture amazing photos of your subject at all times, even in low-light conditions",
+    "category": "camera",
+    "image": "https://www.reliancedigital.in/medias/Qubo-QUBO-BABY-CAM-Smart-Cameras-491936008-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3w0MzM3NXxpbWFnZS9qcGVnfGltYWdlcy9oOTYvaGRkLzk0MTY2MzkwMjEwODYuanBnfDZiZWRkYTc2YzFlODk0MjYwNjc1ZWIyODhmOWIzMzlhMjJlMmJkNmE2ZDE5NjFmNWUyNTI4NzVmMjA1Mzk3NDQ",
+    "rating": {
+      "rate": 3.9,
+      "count": 120
+    }
+  },
+*/
--- reliance_digital_clone/src/Pages/RemoveProduct.jsx
+++ reliance_digital_clone/src/Pages/RemoveProduct.jsx
@@ -1,11 +1,87 @@
 //Bhavik Dholu fw20_0582
+import React from "react";
+import { Box, Flex, FormControl, FormLabel, Heading, Select, Table, TableCaption, TableContainer, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react";
+import {DeleteIcon} from "@chakra-ui/icons";
 
-import React from 'react'
 
 function RemoveProduct() {
+  const [category, setCategory] = React.useState("mobile");
   return (
-    <div>RemoveProduct</div>
-  )
+    <Box mt={5}>
+      <Heading>Remove Product</Heading>
+      <Box
+        w="80%"
+        margin="auto"
+        border="1px solid gray"
+        borderRadius={10}
+        p={5}
+        mt={5}
+      >
+        <FormControl>
+          <FormLabel>Select By Category</FormLabel>
+          <Select placeholder="Select category">
+            <option value={"mobile"}>Mobile</option>
+            <option value={"mens Clothes"}>Mens Clothes</option>
+            <option value={"womens Clothes"}>Womens Clothes</option>
+            <option value={"shoes"}>Shoes</option>
+          </Select>
+        </FormControl>
+      </Box>
+      <Box color='white' w="80%" margin="auto" mt={10} >
+      <Flex gap="20px">
+        <Box
+          align={"center"}
+          justifyContent="space-between"
+          bg='blue.400'
+          p={3}
+          borderRadius="10px"
+        >
+          <Box alignContent='center'>
+            <Text>Category :- {category}</Text>
+          </Box>
+        </Box>
+        <Box
+          align={"center"}
+          justifyContent="space-between"
+          p={3}
+          bg='blue.400'
+          borderRadius="10px"
+        >
+          <Box>
+            <Text>Total Products</Text>
+            <Text>0</Text>
+          </Box>
+        </Box>
+      </Flex>
+      </Box>
+      <Box w='80%' margin='auto'>
+        <TableContainer mt="10px">
+          <Table variant="striped" colorScheme="red">
+            <TableCaption>Recent Orders</TableCaption>
+            <Thead>
+              <Tr bg="teal.300">
+                <Th>Product ID</Th>
+                <Th>Product Name</Th>
+                <Th>Remove</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td>1</Td>
+                <Td>Qubo Baby Cam Smart Camera, Black (Penguin Themed)</Td>
+                <Td><DeleteIcon/></Td>
+              </Tr>
+              <Tr>
+                <Td>1</Td>
+                <Td>Qubo Baby Cam Smart Camera, Black (Penguin Themed)</Td>
+                <Td><DeleteIcon/></Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </TableContainer>
+      </Box>
+    </Box>
+  );
 }
 
-export default RemoveProduct
+export default RemoveProduct;
