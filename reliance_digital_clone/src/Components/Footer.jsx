65c91bd54fe93309a05c349f04499b81889db2bf
atheist31 <rkb0808@gmaol.com>
2022-12-18 04:03:17 +0530

    added d5

--- reliance_digital_clone/src/Components/Footer.jsx
+++ reliance_digital_clone/src/Components/Footer.jsx
@@ -1,113 +1,124 @@
-
 import React from "react";
 // import './App.css';
 
-
-const Footer=()=>{
-    return(
-        <>
-        <footer>
-
-            <div className="container">
-                <div className="row">
-                    <div className="col-12 col-lg-10 mx-auto">
-                        <div className="row">
-                            <div className="foot">
-                                <div className="footers">
-                            <div className="col-9 col-lg-3">
-                                <h2>PRODUCT CATEGORIES</h2>
-                                <ul>
-                                    <p>SmartPhones</p>
-                                    <p>Laptops</p>
-                                    <p>DSLR Cameras</p>
-                                    <p>Television</p>
-                                    <p>Air Conditioners</p>
-                                    <p>personal care & Grooming</p>
-                                    <p>Refrigerators</p>
-                                    <p>Accessories</p>
-                                   
-                                    
-                                </ul>
-
-                            </div>
-
-                            <div className="col-9 col-lg-3">
-                                <h2> SITE INFO</h2>
-                                <ul>
-                                    <p>About reliance Digital</p>
-                                    <p>resQ Services</p>
-                                    <p> Site Map</p>
-                                    <p>Gift Cards</p>
-                                    <p>Corporate Enquires </p>
-                                    <p>Contact Us</p>
-                                </ul>
-
-                            </div>
-
-                            <div className="col-9 col-lg-3">
-                                <h2> Resource Center</h2>
-                                <ul>
-                                    <p>Product Reviews</p>
-                                    <p>Buying Guides</p>
-                                    <p>How Tos</p>
-                                    <p>Featured Stories</p>
-                                    <p>Events & happenings </p>
-                                </ul>
-                                </div>
-
-
-                                <div className="col-9 col-lg-3">
-                                <h2> POLICIES</h2>
-                                <ul>
-
-                                       <p>Terms of use</p>
-                                   
-                                        <p> FAQs</p>
-                                   
-                                        <p >privacy policu </p>
-                                   
-                                        <p>Caution Notice</p>
-                                    
-                                        <p>E-waste policy </p>
-                                   
-                                </ul>
-                                </div>
-                                </div>
-                                    <div className="row">
-                                        <div className="row-3 mx-auto" >
-                                            <h2>Follow us</h2>
-                                            <img style={{width:"70px",height:"30px",margin:"20px"}} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcIKtpPQND6s7iW8wFy8-C77PWYJ7m49FrUw&usqp=CAU"alt="" />
-                                            <img style={{width:"70px",height:"30px",margin:"20px"}}  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLFzDoUTxXStvqtcsyi3S9e1oSVliP53ZS6w&usqp=CAU" alt="" />
-                                            <img style={{width:"70px",height:"30px",margin:"20px"}} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNshlzox4zGs2_nBKtlT5Qnce2PMBG8n_Xtw&usqp=CAU"  alt=""/>
-
-                                        </div>
-
-                                <h2>EXPERIENCE RELIANCE DIGITAL APP ON MOBILE</h2>
-                                    </div>
-
-                            </div>
-
-
-                        </div>
-
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <div className="container">
+          <div className="row">
+            <div className="col-12 col-lg-10 mx-auto">
+              <div className="row">
+                <div className="foot">
+                  <div className="footers">
+                    <div className="col-9 col-lg-3">
+                      <h2>PRODUCT CATEGORIES</h2>
+                      <ul>
+                        <p>SmartPhones</p>
+                        <p>Laptops</p>
+                        <p>DSLR Cameras</p>
+                        <p>Television</p>
+                        <p>Air Conditioners</p>
+                        <p>personal care & Grooming</p>
+                        <p>Refrigerators</p>
+                        <p>Accessories</p>
+                      </ul>
                     </div>
-                    <div className="dis">
-                    <h1>Discliminer</h1>
-                    <p>Product prices, offers and availability are subject to change from time to time. 
-                        All prices are inclusive of taxes. Product colours & images are only for 
-                        illustration and they may not exactly match with the actual product. Product 
-                        specs are subject to change & may vary from actual product. While every care 
-                        is taken to avoid inaccuracies in content, these are provided as is, without
-                         warranty of any kind.</p><hr />
-                         <h4>@ 2022 Reliance Digital .All Rights Reserved.</h4>
-                         </div> 
+
+                    <div className="col-9 col-lg-3">
+                      <h2> SITE INFO</h2>
+                      <ul>
+                        <p>About reliance Digital</p>
+                        <p>resQ Services</p>
+                        <p> Site Map</p>
+                        <p>Gift Cards</p>
+                        <p>Corporate Enquires </p>
+                        <p>Contact Us</p>
+                      </ul>
+                    </div>
+
+                    <div className="col-9 col-lg-3">
+                      <h2> Resource Center</h2>
+                      <ul>
+                        <p>Product Reviews</p>
+                        <p>Buying Guides</p>
+                        <p>How Tos</p>
+                        <p>Featured Stories</p>
+                        <p>Events & happenings </p>
+                      </ul>
+                    </div>
+
+                    <div className="col-9 col-lg-3">
+                      <h2> POLICIES</h2>
+                      <ul>
+                        <p>Terms of use</p>
+
+                        <p> FAQs</p>
+
+                        <p>privacy policy </p>
+
+                        <p>Caution Notice</p>
+
+                        <p>E-waste policy </p>
+                      </ul>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="row-3 mx-auto">
+                      <h2>Follow us</h2>
+                      <img
+                        style={{
+                          width: "70px",
+                          height: "30px",
+                          margin: "20px",
+                        }}
+                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcIKtpPQND6s7iW8wFy8-C77PWYJ7m49FrUw&usqp=CAU"
+                        alt=""
+                      />
+                      <img
+                        style={{
+                          width: "70px",
+                          height: "30px",
+                          margin: "20px",
+                        }}
+                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLFzDoUTxXStvqtcsyi3S9e1oSVliP53ZS6w&usqp=CAU"
+                        alt=""
+                      />
+                      <img
+                        style={{
+                          width: "70px",
+                          height: "30px",
+                          margin: "20px",
+                        }}
+                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNshlzox4zGs2_nBKtlT5Qnce2PMBG8n_Xtw&usqp=CAU"
+                        alt=""
+                      />
+                    </div>
+
+                    <h2>EXPERIENCE RELIANCE DIGITAL APP ON MOBILE</h2>
+                  </div>
                 </div>
+              </div>
             </div>
-        </footer>
-        
-        </>
-       
-    )
-}
+            <div className="dis">
+              <h1>Disclaimer</h1>
+              <p>
+                Product prices, offers and availability are subject to change
+                from time to time. All prices are inclusive of taxes. Product
+                colours & images are only for illustration and they may not
+                exactly match with the actual product. Product specs are subject
+                to change & may vary from actual product. While every care is
+                taken to avoid inaccuracies in content, these are provided as
+                is, without warranty of any kind.
+              </p>
+              <hr />
+              <h4>@ 2022 Reliance Digital .All Rights Reserved.</h4>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
 
 export default Footer;
---
+++ reliance_digital_clone/src/Pages/Home/BootstrapCarousel.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import { Carousel} from "react-bootstrap"
+
+// import Carousel from 'react-bootstrap/Carousel';
+
+export default function BootstrapCarousel() {
+  return (
+    <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://www.reliancedigital.in/medias/Laptop-Carnival-Carousel-Banner-D.jpg?context=bWFzdGVyfGltYWdlc3w5MTcwNXxpbWFnZS9qcGVnfGltYWdlcy9oM2UvaDJiLzk5MzA5MTgwNjgyNTQuanBnfDM0NDIxMjI1ZmE2NjQ1MDk4MWY2MjJlMjU2Mjg0YzAxNDViYTRiM2YzMTEyYjU5OGUwNTZlNDY4NzcxYzk3M2Y"
+          alt="First slide"
+        />
+        {/* <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption> */}
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://www.reliancedigital.in/medias/Up-to-750-Off-Weekend-Offer-Carousel-Banner-16-12-2022.jpg?context=bWFzdGVyfGltYWdlc3wxMTAxNDF8aW1hZ2UvanBlZ3xpbWFnZXMvaDFjL2gxNC85OTMyNTk4NjczNDM4LmpwZ3wwMjUxNzg4MzFhN2RhZDhmM2I0MDk2YzFjMDI1ZDAxNjNiNWY3YzVlMWE4MTE3ODA1YzY4YTFiMDBiZjQxMWEx"
+          alt="Second slide"
+        />
+
+        {/* <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption> */}
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://www.reliancedigital.in/medias/Apple-S8-CLP-Banner-14-11-2022.jpg?context=bWFzdGVyfGltYWdlc3w5NzY3M3xpbWFnZS9qcGVnfGltYWdlcy9oNjAvaDBkLzk5MzExODU1MjA2NzAuanBnfDA3MWZiOGZlMzdjOTYzZDFhZGUyZWMzMjdmOTgxZmJjOGY1MTI2OGI0ZjA3YjU1YzUzNDllZjlkMmQwMmFkMjA"
+          alt="Third slide"
+        />
+
+        {/* <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption> */}
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://www.reliancedigital.in/medias/Accessories-Fiesta-Carousel-Banner-D.jpg?context=bWFzdGVyfGltYWdlc3w5MTAwN3xpbWFnZS9qcGVnfGltYWdlcy9oY2MvaDI5Lzk5Mjk4MTAyMTQ5NDIuanBnfGUxYTc3MTMzNThhNzlkZGQyNTNlYTYxMzM3OWM4NzQzYjY2YWYwYzFkNjhhMjA5M2YwNDhiYTMwMzA2ZGY5MTY"
+          alt="First slide"
+        />
+        {/* <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption> */}
+      </Carousel.Item>
+
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://www.reliancedigital.in/medias/Smartwatch-Carnival-Carousel-Banner-D.jpg?context=bWFzdGVyfGltYWdlc3w5MTMyMnxpbWFnZS9qcGVnfGltYWdlcy9oYmYvaGFmLzk5Mjk4MzI1NjI3MTguanBnfDBkOTJjZDk5YTI1MTU3NWU5MDc1NGQ0ZDg2YjFlOWRhOGRhN2FiMzgxMDY3YWI5Y2M1NDk0MjNjZjU1MzNjYjY"
+          alt="First slide"
+        />
+        {/* <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption> */}
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+// export default UncontrolledExample;
